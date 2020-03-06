c62bbac5b5231bee1071036dc57eb9de7be4cd71
pokeybill <bill@pokeybill.us>
2020-03-05 23:19:11 -0600

    Exposed most config options to the local environment, updated README

--- README.md
+++ README.md
@@ -1,8 +1,26 @@
 # LAOBot
+last updated v0.0.1
+
+## Configuration
+The following values may be set in the environment to configure LAOBot 
+
+* LAOBOT_TEST (default=0/False)
+* LAOBOT_REDIS_DB (default=1)
+* LAOBOT_REDIS_HOST (default=localhost)
+* LAOBOT_REDIS_PORT (default=6379)
+* LAOBOT_CORPUS_DIR - directory containing the television script text files from which the bot generates and detects content
+* LAOBOT_DB_URL (default=sqlite:///test.sqlite)
+* LAOBOT_ACTION_INTERVAL
+* LAOBOT_SERVER_HOST - host used by the LAOBot server (default=localhost)
+* LAOBOT_SERVER_PORT - sets the local port over which the LAOBot server will listen
+
+Reddit strongly encourages informative user-agent strings.  The following options will drive the user-agent string used by LAOBot
+* LAOBOT_AUTHOR (provide this as a reddit user, e.g. **"u/pokeybill"**)
+* LAOBOT_HOMEPAGE
 
 ## Running the bot
-1) Start Redis
-2) Start celery workers
+* Start Redis
+* Start celery workers
   * celery -A laobot.utils.celery worker --loglevel=debug -P solo
-3) Start LAOBot server (blocking command)
+* Start LAOBot server (blocking command)
   * laobot start
--- laobot/utils/constants.py
+++ laobot/utils/constants.py
@@ -14,18 +14,21 @@ TEST = bool(int(os.environ.get('LAOBOT_TEST', 0)))
 
 # Bot activity tuning
 #ACTION_WAIT_TIME = 30 if TEST else (60 * 60 * 6)
-ACTION_WAIT_TIME = 30
-SERVER_HOST = 'localhost'
-SERVER_PORT = 9999
+ACTION_WAIT_TIME = int(os.environ.get('LAOBOT_ACTION_INTERVAL', 30))
+
+SERVER_HOST = os.environ.get('LAOBOT_SERVER_HOST', 'localhost')
+SERVER_PORT = int(os.environ.get('LAOBOT_SERVER_PORT', 9999))
 PIDFILE = os.path.abspath(os.path.join(tempfile.gettempdir(), 'laobot.pid'))
 THREADS_PER_RUNNER = 2
-CORPUS_DIRECTORY = r'Z:\Git\untitled1\data\lao'
+CORPUS_DIRECTORY = os.environ.get('LAOBOT_CORPUS_DIR', r'Z:\Git\untitled1\data\lao')
 
 CELERY_REDIS_DB = os.environ.get('LAOBOT_REDIS_DB', '1')
-CELERY_REDIS_URL = f'redis://localhost:6379/{CELERY_REDIS_DB}'
+redis_host = os.environ.get('LAOBOT_REDIS_HOST', 'localhost')
+redis_port = os.environ.get('LAOBOT_REDIS_PORT', '6379')
+CELERY_REDIS_URL = f'redis://{redis_host}:{redis_port}/{CELERY_REDIS_DB}'
 CELERY_BROKER_URL = CELERY_REDIS_URL
 CELERY_RESULT_BACKEND = CELERY_REDIS_URL
 
-DB_URL = 'sqlite:///test.sqlite'
+DB_URL = os.environ.get('LAOBOT_DB_URL', 'sqlite:///test.sqlite')
 engine = db.create_engine(DB_URL)
 Session = sessionmaker(bind=engine)
